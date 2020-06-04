8aa08c3fe2cb3fb47293bd16b063825d77e3e643
Danny Hua <zaapen@gmail.com>
2020-06-03 20:50:51 -0700

    moved directory and shop data into its own reducer, added collection-overview component and change shop data into an object

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';  //need to install redux
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';  //for debugging
 
 import rootReducer from './root-reduce';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
